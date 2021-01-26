e9fae47041a9e4e64334a4ec2ad6d1718fbf790d
roja <nahomi.ortiz@alumnos.udg.mx>
2021-01-25 21:24:47 -0600

    hola

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+      title: "Tótec",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Tótec"),
+        ),
+        body: new Container(child: new Center(child: new Text("Los tkm"))),
+      )));
+}
